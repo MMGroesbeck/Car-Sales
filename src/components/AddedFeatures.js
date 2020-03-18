8dff25943e59b63f1a3c1926efeea1e37ff43096
M Groesbeck <m.groesbeck@gmail.com>
2020-03-18 09:43:26 -0700

    MVP met.

--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from "react-redux";
 
 import AddedFeature from './AddedFeature';
 
@@ -6,10 +7,10 @@ const AddedFeatures = props => {
   return (
     <div className="content">
       <h6>Added features:</h6>
-      {props.car.features.length ? (
+      {props.currentFeatures.length ? (
         <ol type="1">
-          {props.car.features.map(item => (
-            <AddedFeature key={item.id} feature={item} />
+          {props.currentFeatures.map(item => (
+            <AddedFeature key={item} feature={item} />
           ))}
         </ol>
       ) : (
@@ -19,4 +20,10 @@ const AddedFeatures = props => {
   );
 };
 
-export default AddedFeatures;
+const mapStateToProps = state => {
+  return {
+    currentFeatures: state.car.features
+  }
+}
+
+export default connect(mapStateToProps, {})(AddedFeatures);
